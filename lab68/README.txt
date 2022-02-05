9258b9e0a456a7e0edc874710aacae32882f5b59
Diana Kolpakova <45013798+Darracuda@users.noreply.github.com>
2022-02-05 18:37:04 +0300

    Delete lab68 directory

--- lab68/README.txt
+++
@@ -1,2 +0,0 @@
-g++ node.cpp WorkingNode.cpp -o node
-g++ main.cpp TopNode.cpp execute.cpp IpPortDealer.cpp -o main
--- lab68/execute.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-using namespace std;
-
-int execute(const string& program, const int parametersLength, const string parameters[]);
-
